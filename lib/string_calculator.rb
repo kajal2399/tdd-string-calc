707d0ddefd31c15b1ae211c213bc3d32276509da
kajal2399 <kajalsharma7688@gmail.com>
2024-05-28 22:55:43 +0530

    implemented Step 4 and Step 5 : Support different delimiters & Calling add with a negative number will throw an exception

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,20 @@
 class StringCalculator
-    def add(numbers)
-      return 0 if numbers.empty?
-      numbers.split(/,|\n/).map(&:to_i).reduce(0, :+)
+  def add(numbers)
+    return 0 if numbers.empty?
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
     end
+
+    number_list = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = number_list.select { |num| num < 0 }
+
+    if negatives.any?
+      raise "negative numbers not allowed: #{negatives.join(',')}"
+    end
+
+    number_list.reduce(0, :+)
+  end
 end
--- spec/lib/string_calculator_spec.rb
+++ spec/lib/string_calculator_spec.rb
@@ -17,17 +17,27 @@ RSpec.describe StringCalculator, type: :class do
       calculator = StringCalculator.new
       expect(calculator.add("1,5")).to eq(6)
     end
-  end
+    
+    it 'returns the sum of multiple numbers' do
+        calculator = StringCalculator.new
+        expect(calculator.add("1,2,3")).to eq(6)
+        expect(calculator.add("1,2,3,4,5")).to eq(15)
+    end
 
-  it 'returns the sum of multiple numbers' do
-    calculator = StringCalculator.new
-    expect(calculator.add("1,2,3")).to eq(6)
-    expect(calculator.add("1,2,3,4,5")).to eq(15)
-  end
+    it 'returns the sum of numbers separated by new lines' do
+        calculator = StringCalculator.new
+        expect(calculator.add("1\n2,3")).to eq(6)
+    end
+    
+    it 'supports custom delimiters' do
+        calculator = StringCalculator.new
+        expect(calculator.add("//;\n1;2")).to eq(3)
+    end
 
-  it 'returns the sum of numbers separated by new lines' do
-    calculator = StringCalculator.new
-    expect(calculator.add("1\n2,3")).to eq(6)
+    it 'raises an exception for negative numbers' do
+        calculator = StringCalculator.new
+        expect { calculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+        expect { calculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2,-3")
+    end
   end
-  
 end
